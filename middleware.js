016e93f7ccb5d3747eca280e01a1ff560ec372db
efmichailidis <emichailidis@sklavenitis.co.gr>
2026-09-24 17:49:40 +0300

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -9,17 +9,17 @@ export default function middleware(request) {
   const ALLOWED_DOMAIN = 'https://app.sklavenitismentor.gr'; // 👈 Το εξωτερικό domain
 
   // --- ΕΛΕΓΧΟΣ 1: Iframe Restrictions για το /viewer.html ---
-  if (pathname === '/' || pathname === '/index.html') {
-    const response = NextResponse.next();
+  // if (pathname === '/' || pathname === '/index.html') {
+  //   const response = NextResponse.next();
 
-    // Ορίζουμε το CSP header χωρίς trailing slash
-    response.headers.set(
-      'Content-Security-Policy',
-      `frame-ancestors 'self' ${ALLOWED_DOMAIN}`
-    );
+  //   // Ορίζουμε το CSP header χωρίς trailing slash
+  //   response.headers.set(
+  //     'Content-Security-Policy',
+  //     `frame-ancestors 'self' ${ALLOWED_DOMAIN}`
+  //   );
 
-    return response;
-  }
+  //   return response;
+  // }
   
   // --- ΕΛΕΓΧΟΣ 2: Basic Auth για το Admin & API ---
   const USERNAME = process.env.ADMIN_USERNAME || 'admin';
